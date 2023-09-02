c2aba65baa396a692b1e2c87db9ea529e1c2ba31
gitanshu18 <lusifer0418@gmail.com>
2023-09-02 15:09:09 +0530

    design template for country details

---
+++ country.js
@@ -0,0 +1,11 @@
+ // const elements
+const body = document.querySelector('body');
+
+// ********* ICONS 
+const moonIcon = document.querySelector('#moon');
+
+// **** btns
+const themeBtn = document.getElementById('change-theme'); 
+
+// **** CONTAINERS
+const headerContainer = document.querySelector('.header');
