54d7741c152f324fd56206f0b502b64b592d6303
gitanshu18 <lusifer0418@gmail.com>
2023-09-02 12:51:43 +0530

    write logic for filtering

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ const searchBar = document.querySelector('.search-bar');
 const searchInput = document.querySelector('#search');
 
 const regionFilterBox = document.querySelector('#regions-options');
+const regionText = document.querySelector('.select-text')
 
-
+let data;
 
 
 
@@ -139,14 +140,42 @@ function renderAllCountries(countryArr){
     })
 }
 
+function renderSearchedCountry(name){
+    countriesContainer.innerHTML = '';
+    const searchName = name[0].toUpperCase() + name.slice(1)
+
+    const couObj = data.find(obj => obj.name == searchName);
+
+    if(couObj == undefined){
+        return
+    }
+
+    const html = `
+        <!-- card item -->
+            <div class="card grid" data-numcode="${couObj.numericCode}">
+                <img src="${couObj.flag}" alt="country flag" >
+                <div class="details">
+                    <h2 class="country-name">${couObj.name}</h2>
+                    <ul class="country-dets">
+                        <li><span class="det-key">Population</span>:<span class="det-value"> ${couObj.population}</span> </li>
+                        <li><span class="det-key">Region</span>: <span class="det-value">${couObj.region}</span> </li>
+                        <li><span class="det-key">Capital</span>: <span class="det-value">${couObj.capital}</span> </li>
+                    </ul>
+                </div>
+            </div>  
+        <!-- card item end -->
+        `
+
+        countriesContainer.insertAdjacentHTML('beforeend', html)
+}
+
+function filterRegion(region){
+    const regData = data.filter(obj => obj.region == region);
+    renderAllCountries(regData)
+}
+
 
 // ***************  LOGIC
-
-themeBtn.addEventListener('click', changeTheme);
-changeRegion.value = 'hide';
-
-changeRegion.addEventListener('click', showRegionFilterBox)
-
 
 // requesting file data
 const request = new XMLHttpRequest();
@@ -155,16 +184,55 @@ request.send();
 
 // retrieving file data
 request.addEventListener('load', ()=>{
-    // JSON.parse(request.responseText).forEach(country =>{
-    //     console.log(country)
-    // });
 
-    // const [data, ...others] = JSON.parse(request.responseText);
-    const data  = JSON.parse(request.responseText);
-    // console.log(data);
-    // console.log(data.flag);
-    renderAllCountries(data)
-    
+    data  = JSON.parse(request.responseText);
 })
+
+
+// event handling
+window.addEventListener('load',()=>{
+    renderAllCountries(data)
+})
+
+themeBtn.addEventListener('click', changeTheme);
+changeRegion.value = 'hide';
+changeRegion.addEventListener('click', showRegionFilterBox);
+searchInput.addEventListener('keydown',(e)=>{
+    if (e.key == 'Enter' && searchInput.value != ''){
+        renderSearchedCountry(searchInput.value)
+    }
+    if(searchInput.value == '') renderAllCountries(data)
+})
+
+// storing card details on click
+countriesContainer.addEventListener('click', (e)=>{
+    const clickedCard = e.target.closest('.card').dataset.numcode;
+    localStorage.setItem('card-numcode', clickedCard);
+})
+
+
+
+regionFilterBox.addEventListener('click', (e)=>{
+    if(e.target.value != undefined){
+        if(e.target.textContent != 'Remove filter'){
+            const region = e.target.textContent
+            regionText.textContent = region;
+            filterRegion(region)
+
+        } else{
+            regionText.textContent = 'Filter by Region';
+            renderAllCountries(data)
+        }
+    }
+    chevIcon.name = "chevron-up-outline"
+    regionFilterBox.style.transform = 'translateY(-1000px)';
+    regionFilterBox.style.opacity= '0';    
+    changeRegion.value = 'hide'
+
+})
+
+
+
+
 
 
